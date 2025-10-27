57bf09bb89c6244cdb27fa4ff0ef33efc54f1dc2
Lourenço António Dala <85746261+LouAntonio@users.noreply.github.com>
2025-10-27 10:52:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,45 @@
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'kutambula-marketplace',
-      short_name: 'kutambula-marketplace',
-      description: 'Um marketplace para empresas e startups publicarem produtos e serviços pode ser uma plataforma poderosa',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'kutambula-marketplace',
+        short_name: 'kutambula-marketplace',
+        description:
+          'Um marketplace para empresas e startups publicarem produtos e serviços pode ser uma plataforma poderosa',
+        theme_color: '#ffffff',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+
+  server: {
+    historyApiFallback: true, // 👈 redireciona qualquer rota para index.html
+  },
 })
